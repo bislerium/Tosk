6d408f3c9a2c944edd84a47dd2fa20858167328c
Bishal Gharti Chhetri <bishal.chhetri@innovatetech.co>
2024-12-27 09:03:26 +0545

    Init Maui Blazor Hybrid

---
+++ src/Tosk/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tosk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
